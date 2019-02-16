335fc9325dba7e15c7e7f7c1f7256864d3c6d65f
tspoff <tspoff@gmail.com>
2019-02-16 13:58:59 -0700

    add user models

---
+++ src/data/MapRepo.ts
@@ -0,0 +1,16 @@
+import { User, BountyMetadata } from '../definitions/entities/entities';
+
+export class BountyMetadataRepo {
+  private data: Map<string>;
+  constructor() {
+    this.data = new Map<string, BountyMetadata>();
+  }
+
+  get(id: string) {
+    return this.data.get(id);
+  }
+
+  set(id: string, data: BountyMetadata) {
+    this.data.set(id, data);
+  }
+}
