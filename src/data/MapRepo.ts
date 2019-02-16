786cebef83077f15a24a27ae099e8fa0de420179
tspoff <tspoff@gmail.com>
2019-02-16 14:08:52 -0700

    data work

--- src/data/MapRepo.ts
+++ src/data/MapRepo.ts
@@ -1,16 +1,26 @@
+import { GeoData } from './AOI_JSON';
 import { User, BountyMetadata } from '../definitions/entities/entities';
 
-export class BountyMetadataRepo {
-  private data: Map<string>;
+export class MapRepo {
+  private data: Map<string, GeoData[]>;
   constructor() {
-    this.data = new Map<string, BountyMetadata>();
+    this.data = new Map<string, GeoData[]>();
   }
 
-  get(id: string) {
-    return this.data.get(id);
+  getMapData(id: string): GeoData[] {
+    return this.data.get(id) || ([] as GeoData[]);
   }
 
-  set(id: string, data: BountyMetadata) {
+  setMapData(id: string, data: GeoData[]) {
     this.data.set(id, data);
+  }
+
+  addDataToMap(id: string, dataToAdd: GeoData[]) {
+    let currentData = this.data.get(id);
+    if (currentData) {
+      currentData.push(...dataToAdd);
+    } else {
+      this.setMapData(id, dataToAdd);
+    }
   }
 }
