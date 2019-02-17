179d551822b6ed724afa662f808c5c30d6eec492
Justin Schuldt <justinschuldt@gmail.com>
2019-02-17 03:39:59 -0700

    hacky zoom animation for BountyPage, styles

--- src/components/BountyMapContainer.tsx
+++ src/components/BountyMapContainer.tsx
@@ -10,7 +10,43 @@ interface IMapContainerProps {
 
 export class BountyMapContainer extends Component<IMapContainerProps> {
   loadGeojson(mapProps: any, map: any) {
-    map.data.loadGeoJson('AOI_EPSG4326.json')
+    const { google } = mapProps
+    map.data.setStyle(() => {
+      return {
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE,
+          fillColor: '#FF5E00',
+          strokeColor: '#5e3509',
+          strokeWeight: 2,
+        },
+
+        fillColor: '#FF5E00',
+        fillOpacity: 0.6,
+        strokeColor: '#5e3509',
+        strokeWeight: 2,
+        scale: 3
+      };
+    });
+    // 🤷‍♀️👨‍🔧
+    setTimeout(() => map.setZoom(7), 1600)
+    setTimeout(() => map.setZoom(8), 2000)
+    setTimeout(() => map.setZoom(9), 2200)
+    setTimeout(() => map.setZoom(10), 2400)
+    setTimeout(() => {
+      // load the bounty regions while zooming in
+      map.data.loadGeoJson('AOI_EPSG4326.json')
+      map.setZoom(11)
+    }, 2600)
+    setTimeout(() => {
+
+      map.setZoom(13)
+    }, 3000)
+    setTimeout(() => {
+      // add markers when zooming is complete
+      map.data.loadGeoJson('PointsClip_EPSG4326.json')
+    }, 3200)
+
+
   }
   render() {
     return (
@@ -25,7 +61,7 @@ export class BountyMapContainer extends Component<IMapContainerProps> {
         </Card>
         <Map
           google={this.props.google}
-          zoom={10}
+          zoom={4}
           initialCenter={{
             lat: -4.03,
             lng: 39.6
